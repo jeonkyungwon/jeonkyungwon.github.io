8794246381e8accfb8d38f6839ede826c651a3df
jkw <ashjkw@gmail.com>
2024-09-02 15:25:09 +0900

    setting : 배포 준비

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>제발 돼돼도대ㅗ대라</h1>
+    </div>
+  );
+}
+
+export default App;
